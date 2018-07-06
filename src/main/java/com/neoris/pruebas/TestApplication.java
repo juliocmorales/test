99936b9cc60e77690eead9da6aec70af73c17ffb
juliocmorales <40004698+juliocmorales@users.noreply.github.com>
2018-07-06 10:35:59 -0500

    Primer commit local

---
+++ src/main/java/com/neoris/pruebas/TestApplication.java
@@ -0,0 +1,12 @@
+package com.neoris.pruebas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestApplication.class, args);
+	}
+}
